3086cc9baa15a8bee00a6ce764cfa7c25cf5b745
alejandro3469 <alejandro.perez3469@gmail.com>
2022-12-13 20:51:13 -0600

    Update article.js

--- pages/article.js
+++ pages/article.js
@@ -4,7 +4,6 @@ async function getData() {
   const res = await fetch("http://localhost:3000/api/articles");
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
-
   // Recommendation: handle errors
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
